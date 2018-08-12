14ba0a931ff9d8e81e402ad01a95774d0d0713a4
ikor <igor_korenovsk@mail.ru>
2018-08-12 23:41:56 +0300

    init commit

---
+++ stoppage.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name:  Stoppage
+Plugin URI: https://github.com/Igor-kor/StopPage.git
+Description: Заглушка для посетителей
+Version: 1
+Author: Игорь Шарангия
+Author URI: https://vk.com/id117766113
+*/
+
+function onlyadmin_func()
+{
+    if (!current_user_can('manage_options')) {
+        ?>
+        <style>
+            #textstop {
+                display: flex;
+                justify-content: center;
+                align-content: space-between;
+                align-items: center;
+                height: 100%;
+                font-size: larger;
+                font-family: "Franklin Gothic Medium", serif;
+            }
+
+            #textmiddle {
+                border: 10px solid #dd5265;
+                border-radius: 7px;
+                padding: 30px;
+                width: 80%;
+            }
+
+            body {
+                background-color: #fdfff8;
+            }
+        </style>
+        </head>
+        <body>
+        <div id="textstop">
+            <div id="textmiddle"> Сайт временно приостановлен по техническим причинам.
+                Мы делаем все возможное чтобы возобновить работу сайта.
+            </div>
+        </div>
+        </body>
+        </html>
+        <?php
+        wp_die("", 503);
+    }
+}
+
+add_action('get_header', 'onlyadmin_func');
+?>
